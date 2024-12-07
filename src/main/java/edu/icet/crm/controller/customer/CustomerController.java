1f668833e1ca91204c36b1331076242052a0de1b
DDG000 <dilshanguruge99930@gmail.com>
2024-12-07 12:44:47 +0530

    product CRUD done

---
+++ src/main/java/edu/icet/crm/controller/customer/CustomerController.java
@@ -0,0 +1,8 @@
+package edu.icet.crm.controller.customer;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+}
---
+++ src/main/java/edu/icet/crm/model/Customer.java
@@ -0,0 +1,18 @@
+package edu.icet.crm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Customer {
+
+    private Long ID;
+    private String name;
+    private String contactNumber;
+    private String city;
+}
